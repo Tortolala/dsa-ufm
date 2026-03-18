5bf0605dd70f8ce53f6d4cf48889d6e144c1fd65
L. Angel Tórtola <tortola@ufm.edu>
2026-03-18 07:43:57 -0600

    Linked Lists

--- 05_linked_list/ll.py
+++ 05_linked_list/ll.py
@@ -1,6 +1,4 @@
-'''
-Custom Linked List implementation.
-'''
+''' Custom Linked List implementation. '''
 
 
 class Node:
@@ -9,7 +7,7 @@ class Node:
         self.next = None
 
     def __repr__(self):
-        return f'(DATA: {self.data} | NEXT: {self.next})'
+        return f'(DATA: {self.data} | NEXT: {self.next})' # TODO: En el futuro arreglar repr para mostrar solo la data, error cuando next es None 
 
 
 class LinkedList:
@@ -17,4 +15,42 @@ class LinkedList:
         self.start = None
 
     def __repr__(self):
-        return f'| Start: {self.start} |'      
+        nodes = ['START']
+        for node in self:
+            nodes.append(node.data)
+        nodes.append('NIL')
+        return '\n' + ' --> '.join(nodes)
+
+    def __iter__(self):
+        node = self.start
+        while node is not None:
+            yield node
+            node = node.next
+
+    def __len__(self):
+        length = 0
+        for _ in self:
+            length += 1
+        return length
+
+    def traverse(self):
+        for node in self:
+            print(node.data)
+
+    def insert_at_beginning(self, element: Node):
+        element.next = self.start
+        self.start = element
+
+    def insert_at_end(self, element: Node):
+        pass
+
+    def insert_after_node(self, element: Node, node_reference: any):
+        pass
+    
+    def delete_node(self, element_data: any):
+        # Deletes the first node with matching data
+        pass
+
+    def search(self, element_data: any):
+        # Returns the first node with matching data
+        pass
